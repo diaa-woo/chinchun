b5318cb1136369ba40243c878d440b6612661b16
diaa-woo <dev.dntjr0501@gmail.com>
2024-04-30 13:53:42 +0900

    CHORE :: Add calendar header

--- chinchun/lib/screens/calendar.dart
+++ chinchun/lib/screens/calendar.dart
@@ -18,7 +18,10 @@ class _CalendarPageState extends State<CalendarPage> {
             TableCalendar(
               focusedDay: DateTime.now(),     // NOTE :: 달력에서 자동으로 보여줄 날
               firstDay: DateTime(2024,1,1),   // NOTE :: 달력 구성 시, 맨 처음 보여줄 날짜
-              lastDay: DateTime(2024,12,31)   // NOTE :: 달력 구성 시, 맨 마지막으로 보여줄 날짜
+              lastDay: DateTime(2024,12,31),   // NOTE :: 달력 구성 시, 맨 마지막으로 보여줄 날짜
+              headerStyle: const HeaderStyle(
+                titleCentered: true,
+              ),
             ),
           ],
         ),
