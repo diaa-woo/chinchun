d7237fed4d5410a630b9228144730d3250d2aac9
diaa-woo <dev.dntjr0501@gmail.com>
2024-04-30 15:02:39 +0900

    FEAT :: Add calendar format

--- chinchun/lib/screens/calendar.dart
+++ chinchun/lib/screens/calendar.dart
@@ -9,6 +9,10 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,12 +20,33 @@ class _CalendarPageState extends State<CalendarPage> {
         body: Column(
           children: [
             TableCalendar(
-              focusedDay: DateTime.now(),     // NOTE :: 달력에서 자동으로 보여줄 날
+              focusedDay: _focusedDay,     // NOTE :: 달력에서 자동으로 보여줄 날
               firstDay: DateTime(2024,1,1),   // NOTE :: 달력 구성 시, 맨 처음 보여줄 날짜
               lastDay: DateTime(2024,12,31),   // NOTE :: 달력 구성 시, 맨 마지막으로 보여줄 날짜
               headerStyle: const HeaderStyle(
                 titleCentered: true,
               ),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if(!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
             ),
           ],
         ),
